2ab53a6b0b14110135f5b66a4c5b8ea915cd8857
alf95pagemaster <36070831+alf95pagemaster@users.noreply.github.com>
2018-02-03 20:03:53 +0000

    update

--- contact_form.php
+++ contact_form.php
@@ -1,9 +1,13 @@
-<?PHP
-$to = "barroselasnow@hotmail.com";
-$name = $_POST["name"];
-$email = $_POST["mail"]
-$subject = $_POST["subject"];
-$text = $_POST["text"];
-
-mail($to,$subject,$name,$text,$email);
+<?php
+	$file = 'tentativas_contactos.txt';
+	// Open the file to get existing content
+	$current = file_get_contents($file);
+	// Append a new person to the file
+	$current .= "/n";
+	$current .= "NOME:" + $_POST['name'] + "/n";
+	$current .= "EMAIL:" + $_POST['email'] + "/n";
+	$current .= "ASSUNTO:" + $_POST['subject'] + "/n";
+	$current .= "MENSAGEM:" + $_POST['message'] + "/n";
+	// Write the contents back to the file
+	file_put_contents($file, $current);
 ?>
