09b02a9df6bfdac811efc8dc4eb2189ce03022fd
alf95pagemaster <36070831+alf95pagemaster@users.noreply.github.com>
2018-02-06 21:37:22 +0000

    123

--- js/randomimagechanger.js
+++ js/randomimagechanger.js
@@ -5,21 +5,21 @@
 currentIndx=0;
 
 MyImages=new Array();
-MyImages[0]='images/randomimages/01.jpg';
-MyImages[1]='images/randomimages/02.jpg';
-MyImages[2]='images/randomimages/03.jpg';
-MyImages[3]='images/randomimages/04.jpg';
-MyImages[4]='images/randomimages/05.jpg';
-MyImages[5]='images/randomimages/06.jpg';
-MyImages[6]='images/randomimages/07.jpg';
-MyImages[7]='images/randomimages/08.jpg';
-MyImages[8]='images/randomimages/09.jpg';
-MyImages[9]='images/randomimages/10.jpg';
-MyImages[10]='images/randomimages/11.jpg';
-MyImages[11]='images/randomimages/12.jpg';
-MyImages[12]='images/randomimages/13.jpg';
-MyImages[13]='images/randomimages/14.jpg';
-MyImages[14]='images/randomimages/15.jpg';
+MyImages[0]='images/randomimages/01.png';
+MyImages[1]='images/randomimages/02.png';
+MyImages[2]='images/randomimages/03.png';
+MyImages[3]='images/randomimages/04.png';
+MyImages[4]='images/randomimages/05.png';
+MyImages[5]='images/randomimages/06.png';
+MyImages[6]='images/randomimages/07.png';
+MyImages[7]='images/randomimages/08.png';
+MyImages[8]='images/randomimages/09.png';
+MyImages[9]='images/randomimages/10.png';
+MyImages[10]='images/randomimages/11.png';
+MyImages[11]='images/randomimages/12.png';
+MyImages[12]='images/randomimages/13.png';
+MyImages[13]='images/randomimages/14.png';
+MyImages[14]='images/randomimages/15.png';
  
 
 /* ####################### now we preload the images ####################### */
